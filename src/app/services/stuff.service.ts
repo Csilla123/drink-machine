c057d9098b94b171ce9a5ddad0a857e6c954b843
DESKTOP-8CRD413\Csipike <dfcsilla@gmail.com>
2023-01-16 02:04:27 +0100

    path correction

--- src/app/services/stuff.service.ts
+++ src/app/services/stuff.service.ts
@@ -9,52 +9,52 @@ export class StuffService {
   constructor() {
     this._drinks.push({
       name: "black",
-      src: "./assets/black.png",
+      src: "/assets/black.png",
       cost: 10
     });
     this._drinks.push({
       name: "latte",
-      src: "./assets/latte.png",
+      src: "/assets/latte.png",
       cost: 20
     });
     this._drinks.push({
       name: "cappuccino",
-      src: "./assets/cappuccino.png",
+      src: "/assets/cappuccino.png",
       cost: 30
     });
     this._drinks.push({
       name: "americano",
-      src: "./assets/americano.png",
+      src: "/assets/americano.png",
       cost: 40
     });
     this._drinks.push({
       name: "espresso",
-      src: "./assets/espresso.png",
+      src: "/assets/espresso.png",
       cost: 50
     });
     this._drinks.push({
       name: "doppio",
-      src: "./assets/doppio.png",
+      src: "/assets/doppio.png",
       cost: 60
     });
     this._drinks.push({
       name: "cortado",
-      src: "./assets/cortado.png",
+      src: "/assets/cortado.png",
       cost: 70
     });
     this._drinks.push({
       name: "redeye",
-      src: "./assets/redeye.png",
+      src: "/assets/redeye.png",
       cost: 80
     });
     this._drinks.push({
       name: "lungo",
-      src: "./assets/lungo.png",
+      src: "/assets/lungo.png",
       cost: 90
     });
     this._drinks.push({
       name: "macchiato",
-      src: "./assets/macchiato.png",
+      src: "/assets/macchiato.png",
       cost: 100
     });
   }
